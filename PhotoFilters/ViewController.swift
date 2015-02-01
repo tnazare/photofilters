28c15f081f6b62321e519232bfd47a22d89dd934
Thibaut Nazare <tnazare@gmail.com>
2015-02-01 19:48:38 +0100

    intial commit of the well known photo filter tutorial on swift

--- PhotoFilters/ViewController.swift
+++ PhotoFilters/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var PhotoFilterView: UIImageView!
+    
+    // Create a place to render the image
+    let context = CIContext(options: nil)
+    
+    @IBAction func applyFilter(sender: AnyObject) {
+        //Create the input image
+        let inputImage = CIImage(image: PhotoFilterView.image)
+        
+        //Create a random color to pass to the filter
+        let randomColor = [kCIInputAngleKey:(Double(arc4random_uniform(314))/100)]
+        
+        // Apply a filter to the image
+        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        // Render the filtered image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent())
+        
+        // Applying filtered image
+        PhotoFilterView.image = UIImage(CGImage: renderedImage)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
